c193e6e83645598d4f3c049f27bf1963596a54f1
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-08-04 23:37:33 +0100

    let poetry figure it out

--- start.sh
+++ start.sh
@@ -16,7 +16,6 @@ apt purge -y --auto-remove software-properties-common && rm -rf /var/lib/apt/lis
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 source $HOME/.poetry/env
-poetry env use python3.9
 poetry install
 poetry run python -m pip uninstall torch torchvision torchaudio
 poetry run python -m pip install torch==1.12.0 torchvision==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
