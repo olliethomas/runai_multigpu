8f3260ae89c6a155d9bdd7bf6347af2c2d06321d
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-08-04 18:04:08 +0100

    add poetry to path

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.9 python-is-python3 -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-
+source $HOME/.poetry/env
 poetry install
 poetry run pip uninstall torch torchvision torchaudio
 poetry run pip install torch==1.12.0 torchvision==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
