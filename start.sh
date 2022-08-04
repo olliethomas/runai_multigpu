0e3b7e915400889c78b70119f23f418e77c82731
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-08-04 23:19:39 +0100

    set env python3.9

--- start.sh
+++ start.sh
@@ -3,11 +3,17 @@
 export TZ="Europe/Moscow"
 export DEBIAN_FRONTEND="noninteractive"
 
+pythonVersions='python3.6 python3.7 python3.8 python3.9 python3.10'
+
 apt update && apt upgrade
 apt install curl -y
-apt install software-properties-common -y
+apt install software-properties-common -y --no-install-recommends
 add-apt-repository ppa:deadsnakes/ppa -y
-apt install python3.9 python3.9-distutils python-is-python3 -y
+apt update && apt upgrade
+apt-get install -y --no-install-recommends $pythonVersions
+apt install python3-distutils python-is-python3 -y
+apt-get purge -y --auto-remove software-properties-common && rm -rf /var/lib/apt/lists/*
+
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 source $HOME/.poetry/env
 poetry env use python3.9
