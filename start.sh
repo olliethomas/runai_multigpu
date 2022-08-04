2544569348ef59e9948dc49fb1be4a2b9befd22b
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-08-04 22:59:19 +0100

    set env python3.9

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.9 python3.9-distutils python-is-python3 -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 source $HOME/.poetry/env
+poetry env use python3.9
 poetry install
 poetry run python -m pip uninstall torch torchvision torchaudio
 poetry run python -m pip install torch==1.12.0 torchvision==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
